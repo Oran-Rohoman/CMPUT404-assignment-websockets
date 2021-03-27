6f9c8d29acb5775543508119824d8186d5d29e50
Oran-Rohoman <oran@ualberta.ca>
2021-03-27 05:34:57 -0400

    tests pass and new art

--- sockets.py
+++ sockets.py
@@ -26,6 +26,17 @@ app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
 
+# used from U of A github code.
+#https://github.com/uofa-cmput404/cmput404-slides/blob/master/examples/WebSocketsExamples/broadcaster.py
+
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+
+
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+
 class World:
     def __init__(self):
         self.clear()
@@ -60,29 +71,62 @@ class World:
         return self.space
 
 myWorld = World()        
+clients = list()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
-
+    for client in clients:
+        temp = {}
+        temp[str(entity)] = data
+        client.put(json.dumps(temp))
+    
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect("/static/index.html")
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            msg = ws.receive()
+            print("WS RECV: %s" % msg)
+            if (msg is not None):
+                packet = json.loads(msg)
+                #print(packet)
+                for k,v in packet.items():
+                    myWorld.set(k,v)
+                #print("packet[entity]"+str(packet["entity"]))
+            else:
+                break
+    except:
+        '''Done'''
+        print("error reading")
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
-    '''Fufill the websocket URL of /subscribe, every update notify the
-       websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
-
+    
+    print("Subscribing")
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws,ws, client)
+    ws.send(json.dumps(myWorld.world()))
+    #print("Subscribing")
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            print("Got a message!")
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+    
+    
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -99,25 +143,48 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()
+    if request.method =="PUT":
+        myWorld.set(entity,data)
+
+    else:
+        for k,v in data.items():
+            myWorld.update(entity,k,v)
+    return json.dumps(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    if request.method == 'POST':
+        data = flask_post_json()
+        myWorld.world = data
+        
+    if request.method == 'GET':
+        return json.dumps(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return myWorld.get(entity)
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return myWorld.world()
 
 
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
 
 if __name__ == "__main__":
     ''' This doesn't work well anymore:
@@ -125,4 +192,5 @@ if __name__ == "__main__":
         and run
         gunicorn -k flask_sockets.worker sockets:app
     '''
-    app.run()
+    os.system("gunicorn -k flask_sockets.worker sockets:app")
+    
